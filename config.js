76d4b51fed0791867e46b3d2f82f9a07709284c2
RICKY020-jpeg <darklone020@gmail.com>
2026-01-29 20:48:41 +0000

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dbzvyjxie/video/upload/v1769719656/Jung_Kook_-_Standing_Next_To_You_Lyrics_t5dwyh.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
